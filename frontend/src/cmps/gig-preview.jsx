f3d58f7bf19b23f838a96c7bcaa33dd57d30ebd5
Linoy Alkalay <linoyalkalay@gmail.com>
2023-01-18 13:19:28 +0200

    list gig preview

--- frontend/src/cmps/gig-preview.jsx
+++ frontend/src/cmps/gig-preview.jsx
@@ -3,18 +3,30 @@ import { Link } from 'react-router-dom'
 export function GigPreview({ gig }) {
 
     return <>
-    <div className="img-container">
-        <img src={require('../assets/img/demo.jpg')} alt="" />
-    </div>
-    <div className="content">
-        <span>{gig.owner && gig.owner.fullname}</span>
-        <Link to={`/gig/${gig._id}`}>
-            <span>{gig.title}</span>
-        </Link>
-    </div>
+        <div className="img-container">
+            <img src={require('../assets/img/demo.jpg')} alt="" />
+        </div>
+        <div className="content">
+            <span>{gig.owner && gig.owner.fullname}</span>
+            <Link to={`/gig/${gig._id}`}>
+                <span>{gig.title}</span>
+            </Link>
+            <div className="rating">
+                ★4.9
+            </div>
+        </div>
         <footer>
-            <button className="fa-solid heart"></button>
-            <span>${gig.price}</span>
+            <div className="btn-container">
+                <button className="fa-solid bars"></button>
+                <button className="fa-solid heart"></button>
+            </div>
+            <Link className="price" to={`/gig/${gig._id}`}>
+                <small>Starting at</small>
+                <span>
+                    ${gig.price}
+                    <sup>00</sup>
+                </span>
+            </Link>
         </footer>
     </>
 }
