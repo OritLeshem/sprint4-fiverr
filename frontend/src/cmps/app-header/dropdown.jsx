0aa8ff5204d47c477d86d7c61178ae70645f7500
Linoy Alkalay <linoyalkalay@gmail.com>
2023-01-20 17:40:59 +0200

    dropdown

--- frontend/src/cmps/app-header/dropdown.jsx
+++ frontend/src/cmps/app-header/dropdown.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 
-export function Dropdown({ onLogout }) {
+export function Dropdown({ onLogout, setIsDropdown }) {
 
     return <aside className="dropdown">
         <ul>
-            <li>Profile</li>
+            <li><Link to="/user" onClick={() => setIsDropdown(false)}>Profile</Link></li>
             <li onClick={() => onLogout()}>Logout</li>
         </ul>
     </aside>
