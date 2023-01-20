041eab4824598d1b7cf657bb5908f2f7f862156a
MayanFeldbaum <mayan.lemberg@gmail.com>
2023-01-20 19:30:45 +0200

    edit

--- frontend/src/pages/gig/gig-edit.jsx
+++ frontend/src/pages/gig/gig-edit.jsx
@@ -31,11 +31,9 @@ export function GigEdit() {
     const GigSchema = Yup.object().shape({
         title: Yup.string()
             .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
             .required('Required'),
         description: Yup.string()
             .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
             .required('Required'),
         price: Yup.number().min(1, 'minimum 1$').required('Required'),
         tags: Yup.string()
