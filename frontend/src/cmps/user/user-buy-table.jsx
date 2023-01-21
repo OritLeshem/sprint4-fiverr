59ab3b9a9c9f9b7d8250274b87ccf454180fbad5
OritLeshem <azmono@gmail.com>
2023-01-21 14:15:21 -0500

    byer

--- frontend/src/cmps/user/user-buy-table.jsx
+++ frontend/src/cmps/user/user-buy-table.jsx
@@ -6,51 +6,29 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { userService } from "../../services/user.service"
+import { gigService } from "../../services/gig.service"
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import { loadOrders } from "../../store/order/order.actions"
+
 
 export default function UserBuyTable({ orders }) {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Order</TableCell>
-            <TableCell align="right">Seller</TableCell>
-            <TableCell align="right">Gig title</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {orders.map((order) => (
-            <TableRow
-              key={order._id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {order._id}
-              </TableCell>
+  // const orders = useSelector((storeState) => storeState.orderModule.orders)
+  const user = userService.getLoggedinUser()
 
-              <TableCell align="right">{order.seller.fullname}</TableCell>
-              <TableCell align="right">{order.gig.title}</TableCell>
-              <TableCell align="right">{order.gig.price}</TableCell>
-              <TableCell align="right">{order.status}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+  // useEffect(() => {
+  //   loadOrders(user._id)
+  // }, [])
+  useEffect(() => { }, [])
+  return (
+    <div>
+      {orders.map(order => <div key={order._id}>
+
+      </div>)}
+    </div>
   )
 }
 
--- frontend/src/cmps/user/user-buying-order.jsx
+++ frontend/src/cmps/user/user-buying-order.jsx
@@ -1,3 +1,3 @@
 export function UserBuyingOrder() {
-
+  return <div>hello from buyer</div>
 }
