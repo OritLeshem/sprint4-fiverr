ed33583e0708ca46a8ddc3f6717dfed0e2f8882d
MayanFeldbaum <mayan.lemberg@gmail.com>
2023-01-21 17:57:28 +0200

    user-sales

--- frontend/src/cmps/user/user-sales.jsx
+++ frontend/src/cmps/user/user-sales.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { userService } from "../../services/user.service"
 import { loadOrders } from "../../store/order/order.actions"
@@ -7,39 +7,11 @@ export function UserSales() {
 
   const orders = useSelector((storeState) => storeState.orderModule.orders)
   const user = userService.getLoggedinUser()
+
   useEffect(() => {
-    // if (searchParams.get('category') || searchParams.get('title')) renderUiByQueryStringParams()
     loadOrders(user._id)
-    console.log(orders)
   }, [])
 
-
-  console.log(orders)
   return <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Order id</th>
-          <th>Buyer</th>
-          <th>Gig title</th>
-          <th>Price</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map(order => {
-          return <tr key={order._id}>
-
-            <td>{order._id}</td>
-            <td>{order.buyer.fullname}</td>
-            <td>{order.gig.title}</td>
-            <td>{order.gig.price}</td>
-            <td>{order.status}</td>
-          </tr>
-        })}
-      </tbody>
-    </table>
-
-
   </div>
 }
