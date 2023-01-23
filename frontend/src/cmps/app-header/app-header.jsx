5f8271bc44a01a2acf9e3d765068db0ede496073
Linoy Alkalay <linoyalkalay@gmail.com>
2023-01-23 19:47:59 +0200

    menu

--- frontend/src/cmps/app-header/app-header.jsx
+++ frontend/src/cmps/app-header/app-header.jsx
@@ -98,8 +98,12 @@ export function AppHeader() {
     function onCloseModal() {
         setIsModal(false)
     }
+
+    function onToggleMenu() {
+        
+    }
+
     function handleOrder() {
-        console.log('hi');
         setIsOrder(prev => !prev)
     }
 
@@ -128,10 +132,7 @@ export function AppHeader() {
                             <Link className="user-link fa-regular heart" title="Lists"></Link>
                             <button onClick={handleOrder} className="user-link">Orders</button>
                             {user.imgUrl && <img src={user.imgUrl}
-                                onClick={() => {
-                                    setIsOrder(false)
-                                    setIsDropdown(!isDropdown)
-                                    }} />}
+                                onClick={() => setIsDropdown(!isDropdown)} />}
                             {isDropdown && <Dropdown onLogout={onLogout} setIsDropdown={setIsDropdown} />}
                         </>
                     }
