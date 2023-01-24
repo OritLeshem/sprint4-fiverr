f06a20dd0227e477f3f5a500d660e50ed6a3f963
OritLeshem <azmono@gmail.com>
2023-01-24 07:10:54 -0500

    total statisic

--- frontend/src/cmps/user/user-dashboard/user-seller-table.jsx
+++ frontend/src/cmps/user/user-dashboard/user-seller-table.jsx
@@ -14,11 +14,23 @@ import { updateOrder } from "../../../store/order/order.actions";
 export default function UserSellerTable({ orders }) {
 
   const [isModal, setIsModal] = useState({ id: '', status: false })
-
+  //const [statistic, setStatistic] = useState()
   function toggleStatusModal(orderId) {
     setIsModal(prevModal => ({ ...prevModal, id: orderId, status: !prevModal.status }))
   }
+  let statistic
+  orders && calcAnalysis()
 
+  function calcAnalysis() {
+    console.log("hello from calc")
+
+    statistic = orders.reduce((acc, val) => {
+      acc[val.status] = acc[val.status] ? ++acc[val.status] : 1
+      return acc
+    }, {})
+    console.log(statistic)
+
+  }
   function updateStatus(status, order) {
     order.status = status
     updateOrder(order)
@@ -27,6 +39,25 @@ export default function UserSellerTable({ orders }) {
 
   return (
     <div className="status-options-btn">
+
+      <div className="statstic-show">
+        <div className=" statstic-total ">
+          <h3>Total sales </h3>
+          {statistic && <h3>{orders.length}</h3>}
+        </div>
+        <div className=" statstic-total statstic-completed">
+          <h3>Completed</h3>
+          {statistic && <h3>{statistic.completed}</h3>}
+        </div>
+        <div className=" statstic-total statstic-pending">
+          <h3>Pending</h3>
+          {statistic && <h3>{statistic.pending}</h3>}
+        </div>
+        <div className=" statstic-total statstic-declined">
+          <h3>Declined</h3>
+          {statistic && <h3>{statistic.declined}</h3>}
+        </div>
+      </div>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -52,17 +83,19 @@ export default function UserSellerTable({ orders }) {
                 <TableCell align="right">{order.gig.price}</TableCell>
                 <TableCell align="right">
                   {(isModal.status && isModal.id === order._id) && <div className="status-options">
-                    <button className="pending" onClick={() => updateStatus("Pending", order)}>Pending</button>
-                    <button className="completed" onClick={() => updateStatus("Completed", order)}>Completed</button>
-                    <button className="declined" onClick={() => updateStatus("Declined", order)}>Declined</button>
+                    <button className="pending" onClick={() => updateStatus("pending", order)}>Pending</button>
+                    <button className="completed" onClick={() => updateStatus("completed", order)}>Completed</button>
+                    <button className="declined" onClick={() => updateStatus("declined", order)}>Declined</button>
                   </div>
                   }
-                  <button className={order.status} onClick={() => toggleStatusModal(order._id)}>{order.status}</button></TableCell>
+                  <button className={order.status} onClick={() => toggleStatusModal(order._id)}>{order.status.charAt(0).toUpperCase() + order.status.slice(1)}</button></TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+
     </div>
   )
 }
