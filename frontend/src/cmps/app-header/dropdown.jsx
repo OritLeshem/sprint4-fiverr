3ab5475a099826d5db0fd6061ced5140054b0f49
Linoy Alkalay <linoyalkalay@gmail.com>
2023-01-27 14:58:53 +0200

    dropdown and chart fixed

--- frontend/src/cmps/app-header/dropdown.jsx
+++ frontend/src/cmps/app-header/dropdown.jsx
@@ -5,7 +5,7 @@ export function Dropdown({ onLogout, setIsDropdown, user }) {
     return <aside className="dropdown">
         <ul>
             <li><Link to={`/user/${user._id}`} onClick={() => setIsDropdown(false)}>Profile</Link></li>
-            <li onClick={() => onLogout()}>Logout</li>
+            <li onClick={() => onLogout()}><span>Logout</span></li>
         </ul>
     </aside>
 }
